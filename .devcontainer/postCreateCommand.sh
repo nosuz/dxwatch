86649607100a795496dd1631f34da607171587ae
Norio Suzuki <norisuzu@gmail.com>
2026-04-11 19:07:19 +0900

    Fixed .claude is not preserved.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -12,11 +12,17 @@ if [ -f "$MARKER_FILE" ]; then
   exit 0
 fi
 
-# 初期化完了を示すマーカーファイルを作成
-touch "$MARKER_FILE"
-
-# mkdir -p /workspaces/.codex
-# ln -sfn /workspaces/.codex ~/.codex
+mkdir -p /workspaces/.codex
+if [ -e "$HOME/.codex" ] && [ ! -L "$HOME/.codex" ] ; then
+  mv "$HOME/.codex" "$HOME/.codex.backup.$(date +%Y%m%d%H%M%S)"
+fi
+ln -s /workspaces/.codex ~/.codex
 
 mkdir -p /workspaces/.claude
-ln -sfn /workspaces/.claude ~/.claude
+if [ -e "$HOME/.claude" ] && [ ! -L "$HOME/.claude" ] ; then
+  mv "$HOME/.claude" "$HOME/.claude.backup.$(date +%Y%m%d%H%M%S)"
+fi
+ln -s /workspaces/.claude ~/.claude
+
+# 初期化完了を示すマーカーファイルを作成
+touch "$MARKER_FILE"
